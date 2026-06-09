79fb75447c84d7cb5530f701c16cc7dfe7192275
doyeon-SM <82075191+doyeon-SM@users.noreply.github.com>
2026-06-09 16:08:05 +0900

    26.06.09
    boss entranceobject add

--- Assets/02.Script/Interaction/PlayerMoveInteractionObject.cs
+++ Assets/02.Script/Interaction/PlayerMoveInteractionObject.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerMoveInteractionObject : MonoBehaviour, IInteraction
 {
+    [SerializeField] private Transform targetTransform;
     public void Interaction()
     {
         Debug.Log("Boss Entrance");
