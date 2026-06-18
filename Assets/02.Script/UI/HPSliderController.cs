233de0b84578f0daaa22bbc2615b344569e07a94
sudeang0-hue <sudeang0@gmail.com>
2026-06-18 11:16:53 +0900

    [ 26.06.18 - fead ]HP 슬라이더 데미지 지연 연출 추가
    - HPSliderController에 afterSlider, beforeSlider 분리
    - HP 감소 시 afterSlider를 먼저 갱신하고 beforeSlider는 delayTime 이후 반영
    - valueChangeDuration을 통해 HP 감소 값을 부드럽게 보간
    - 기존 slider Inspector 참조가 afterSlider로 이전되도록 FormerlySerializedAs 적용
    - hpChanged 및 Slider 참조 null 체크 추가

--- Assets/02.Script/UI/HPSliderController.cs
+++ Assets/02.Script/UI/HPSliderController.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace _01.Scenes.PhaseValidation.UI
@@ -6,22 +8,116 @@ namespace _01.Scenes.PhaseValidation.UI
     public class HPSliderController : MonoBehaviour
     {
         [SerializeField] private DoubleIntEventChannel hpChanged;
-        [SerializeField] private Slider slider;
+
+        [Header("HP Sliders")]
+        [FormerlySerializedAs("slider")]
+        [SerializeField] private Slider afterSlider;
+        [SerializeField] private Slider beforeSlider;
+
+        [Header("Damage Animation")]
+        [SerializeField, Min(0f)] private float delayTime = 0.25f;
+        [SerializeField, Min(0f)] private float valueChangeDuration = 0.25f;
+
+        private Coroutine hpChangeCoroutine;
+        private float currentTargetValue = 1f;
+        private bool hasTargetValue;
 
         private void OnEnable()
         {
-            hpChanged.Register(OnHpChanged);
+            if (hpChanged != null)
+                hpChanged.Register(OnHpChanged);
         }
 
         private void OnDisable()
         {
-            hpChanged.Unregister(OnHpChanged);
+            if (hpChanged != null)
+                hpChanged.Unregister(OnHpChanged);
+
+            if (hpChangeCoroutine != null)
+            {
+                StopCoroutine(hpChangeCoroutine);
+                hpChangeCoroutine = null;
+            }
         }
 
-        private void OnHpChanged(int currenthp,int maxHp)
+        private void OnHpChanged(int currentHp, int maxHp)
         {   
-            float hpRatio = (float)currenthp / maxHp;
-            slider.value = hpRatio;
+            float hpRatio = maxHp > 0 ? Mathf.Clamp01((float)currentHp / maxHp) : 0f;
+            float previousTargetValue = hasTargetValue ? currentTargetValue : GetCurrentSliderValue(hpRatio);
+            bool isDamage = hpRatio < previousTargetValue;
+
+            currentTargetValue = hpRatio;
+            hasTargetValue = true;
+
+            if (hpChangeCoroutine != null)
+                StopCoroutine(hpChangeCoroutine);
+
+            hpChangeCoroutine = StartCoroutine(ApplyHpChange(hpRatio, isDamage));
+        }
+
+        private IEnumerator ApplyHpChange(float targetValue, bool useDelay)
+        {
+            float afterStartValue = GetSliderValue(afterSlider, targetValue);
+            float beforeStartValue = GetSliderValue(beforeSlider, afterStartValue);
+            float beforeDelay = useDelay ? Mathf.Max(0f, delayTime) : 0f;
+            float duration = Mathf.Max(0f, valueChangeDuration);
+
+            if (duration <= 0f)
+            {
+                SetSliderValue(afterSlider, targetValue);
+
+                if (beforeDelay > 0f)
+                    yield return new WaitForSeconds(beforeDelay);
+
+                SetSliderValue(beforeSlider, targetValue);
+                hpChangeCoroutine = null;
+                yield break;
+            }
+
+            float elapsedTime = 0f;
+            float totalTime = Mathf.Max(duration, beforeDelay + duration);
+
+            while (elapsedTime < totalTime)
+            {
+                elapsedTime += Time.deltaTime;
+
+                float afterProgress = Mathf.Clamp01(elapsedTime / duration);
+                SetSliderValue(afterSlider, Mathf.Lerp(afterStartValue, targetValue, afterProgress));
+
+                if (elapsedTime >= beforeDelay)
+                {
+                    float beforeProgress = Mathf.Clamp01((elapsedTime - beforeDelay) / duration);
+                    SetSliderValue(beforeSlider, Mathf.Lerp(beforeStartValue, targetValue, beforeProgress));
+                }
+
+                yield return null;
+            }
+
+            SetSliderValue(afterSlider, targetValue);
+            SetSliderValue(beforeSlider, targetValue);
+            hpChangeCoroutine = null;
+        }
+
+        private float GetCurrentSliderValue(float fallbackValue)
+        {
+            if (afterSlider != null)
+                return afterSlider.value;
+
+            if (beforeSlider != null)
+                return beforeSlider.value;
+
+            return fallbackValue;
+        }
+
+        private float GetSliderValue(Slider targetSlider, float fallbackValue)
+        {
+            return targetSlider != null ? targetSlider.value : fallbackValue;
+        }
+
+        private void SetSliderValue(Slider targetSlider, float value)
+        {
+            if (targetSlider != null)
+                targetSlider.value = value;
         }
     }
 }
